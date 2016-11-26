037ee6b49673c408a23daf84e169acf2a7155178
bigding <1037196370@qq.com>
2016-11-26 23:45:22 +0800

    学生添加课程部分完成

--- stu/opAddCourse.php
+++ stu/opAddCourse.php
@@ -1,9 +1,12 @@
 <?php
+session_start();
 $courseNo = $_GET['courseNo'];
 $courseSeqNo = $_GET['courseSeqNo'];
 $pageUserNo = $_SESSION['stuNo'];
 
-echo $_SESSION['stuNo'];
+//echo $_SESSION['stuNo'];
+
+//echo print_r($_SESSION);
 
 require 'mysqlConfig.php';
 $sql1 = "select courseTime,classroom from course_info where courseNo ='$courseNo' and courseSeqNo='$courseSeqNo'";
@@ -16,17 +19,16 @@ else {
     while ($row1 = mysqli_fetch_assoc($result1)){
         $courseTime = $row1['courseTime'];
         $classroom = $row1['classroom'];
-        $sql2 = "select courseName from course_info 
-                  where choice_info.courseNo = course_info.courseNo and
-                  choice_info.courseSeqNo = course_info.courseSeqNo and
+        $sql2 = "select courseName from course_info,choice_info
+                  where course_info.courseNo = choice_info.courseNo and
+                  course_info.courseSeqNo = choice_info.courseSeqNo and
                   choice_info.stuNo = '$pageUserNo' and
                   course_info.courseTime = '$courseTime' and 
                   course_info.classroom = '$classroom'";
-        echo $sql2."<br/>";
         $result2 = mysqli_query($conn,$sql2);
         if(mysqli_num_rows($result2) > 0){
-            while($row2 = $result2){
-                echo "你先添加的课程和您已有的课程冲突(".$row2['courseName'].")";
+            while($row2 = mysqli_fetch_array($result2)){
+                echo "你现在添加的课程和您已有的课程冲突(".$row2['courseName'].")";
                 $status = false;
                 break;
             }
@@ -37,9 +39,11 @@ else {
     if($status){
        $sql3 = "insert into choice_info (courseNo,courseSeqNo,stuNo) 
                 values ('$courseNo','$courseSeqNo','$pageUserNo')";
+        mysqli_query($conn,$sql3);
         if(mysqli_affected_rows($conn) == 1){
             echo '添加课程成功';
         }
     }
+    echo $status;
 }
 ?>
--- stu/stuLogIn.php
+++ stu/stuLogIn.php
@@ -11,7 +11,6 @@ $password = trim(HTMLSpecialchars($password));
 
 require "mysqlConfig.php";
 $sql = "select name from student_info WHERE stuNo= '$stuNo' and password = '$password'";
-echo $sql . "<br>";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 0) {
     die("不存在此用户，请重新输入信息确认");
@@ -22,12 +21,12 @@ if (mysqli_num_rows($result) == 0) {
         $pageuser = $row["name"];
         $_SESSION["pageuser"] = $row['name'];
         $_SESSION["stuNo"] = $stuNo;
-        print_r($_SESSION);
         if ($_SESSION["pageuser"] != '' && $_SESSION["stuNo"] != '') {
             echo "成功登陆";
+            echo $_SESSION["pageuser"]." ".$_SESSION["stuNo"]."<br/>";
             echo "<script>
-            window.location.href='information.php';
-        </script>";
+                window.location.href='information.php';
+            </script>";
         } else {
             session_destroy();
             echo "登录失败";
