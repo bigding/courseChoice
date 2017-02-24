f6b221709d2a790d48e05bcfb3a0ea058b80ff82
bigding <tiandingdingding@gmail.com>
2017-02-24 11:21:39 +0800

    修改由于git引起的代码错误

--- stu/stuLogIn.php
+++ stu/stuLogIn.php
@@ -9,15 +9,8 @@ $stuPassword = $_POST["password"];
 /*此处验证学号的正确性并进行处理*/
 $stuNo = trim(HTMLSpecialchars($stuNo));
 $stuPassword = trim(HTMLSpecialchars($stuPassword));
-echo '==.';
 require "mysqlConfig.php";
 $sql = "select name from student_info WHERE stuNo= '$stuNo' and password = '$stuPassword'";
-<<<<<<< HEAD
-echo $sql;
-echo '-.-';
-=======
-//echo $sql;
->>>>>>> 63f680e139178e332f9c17c2b5e27154939aa4e8
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 0) {
     die("不存在此用户，请重新输入信息确认");
@@ -31,7 +24,6 @@ if (mysqli_num_rows($result) == 0) {
         $_SESSION["stuNo"] = $stuNo;
         if ($_SESSION["pageuser"] != '' && $_SESSION["stuNo"] != '') {
             echo "成功登陆";
-//            echo $_SESSION["pageuser"]." ".$_SESSION["stuNo"]."<br/>";
             echo "<script>
                 window.location.href='information.php';
             </script>";
